a11d4c0231da483d4907a4c01cf33c9110f7656a
Eric B <eric_baffour-addo@alumni.brown.edu>
2022-08-11 01:27:12 -0700

    Update all media_id to a type of text

--- src/db/lights-off.sql
+++ src/db/lights-off.sql
@@ -11,12 +11,12 @@ VALUES('film'),('tv');
 /* create media_source table */
 CREATE TABLE media_source(
     media_source_id SERIAL PRIMARY KEY UNIQUE NOT NULL,
-    source TEXT NOT NULL
-)
+    source_type TEXT NOT NULL
+);
 
 /* insert into media_source table*/
-INSERT INTO media_source(source)
-VALUES('imdb'),('user')
+INSERT INTO media_source(source_type)
+VALUES('imdb'),('user');
 
 /* create user_account table*/
 CREATE TABLE user_account(
@@ -25,7 +25,15 @@ CREATE TABLE user_account(
     username TEXT UNIQUE NOT NULL CHECK(LENGTH(username)>=4),
     password TEXT NOT NULL CHECK(LENGTH(password)>=4),
     image TEXT
-)
+);
+
+CREATE TABLE user_account(
+    user_account_ID SERIAL PRIMARY KEY UNIQUE,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    username TEXT UNIQUE NOT NULL CHECK(LENGTH(username)>=4),
+    password TEXT NOT NULL CHECK(LENGTH(password)>=4),
+    image TEXT
+);
 
 /*create media table*/
 CREATE TABLE media(
@@ -45,33 +53,32 @@ CREATE TABLE media(
     imdb TEXT,
     CHECK(NOT(media_source_id=2 AND user_account_id IS NULL)),
     CHECK(NOT(media_source_id=2 AND date_uploaded IS NULL))
-)
+);
 
 CREATE TABLE user_favorite(
     user_account_id SERIAL NOT NULL REFERENCES user_account(user_account_id),
-    media_id SERIAL NOT NULL REFERENCES media(media_id) ON DELETE CASCADE,
+    media_id TEXT NOT NULL REFERENCES media(media_id) ON DELETE CASCADE,
     PRIMARY KEY(user_account_id,media_id)
-)
+);
 
 CREATE TABLE user_seen(
     user_account_id SERIAL NOT NULL REFERENCES user_account(user_account_id),
-    media_id SERIAL NOT NULL REFERENCES media(media_id) ON DELETE CASCADE,
-    date_seen DATE NOT NULL DEFAULT CURRENT_DATE
+    media_id TEXT NOT NULL REFERENCES media(media_id) ON DELETE CASCADE,
+    date_seen DATE NOT NULL DEFAULT CURRENT_DATE,
     PRIMARY KEY(user_account_id,media_id)
-)
+);
 
 CREATE TABLE user_watch_next(
     user_account_id SERIAL NOT NULL REFERENCES user_account(user_account_id),
-    media_id SERIAL NOT NULL REFERENCES media(media_id) ON DELETE CASCADE,
+    media_id TEXT NOT NULL REFERENCES media(media_id) ON DELETE CASCADE,
     priority_level TEXT CHECK(priority_level IN ('low','medium','high')),
     PRIMARY KEY(user_account_id,media_id)
-)
+);
 
 CREATE TABLE user_review(
     user_account_id SERIAL NOT NULL REFERENCES user_account(user_account_id),
-    media_id SERIAL  NOT NULL REFERENCES media(media_id) ON DELETE CASCADE,
+    media_id TEXT  NOT NULL REFERENCES media(media_id) ON DELETE CASCADE,
     review TEXT NOT NULL,
     rating SMALLINT NOT NULL,
     PRIMARY KEY(media_id,user_account_id)
-)
-
+);

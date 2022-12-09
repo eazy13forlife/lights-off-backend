a23510948b7ffb1d616cf46c8bb463b46ad364c3
Eric B <eric_baffour-addo@alumni.brown.edu>
2022-12-09 03:46:13 -0800

    Update port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const port = process.env.PGPORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Lights off backend has started running on port ${port}`);

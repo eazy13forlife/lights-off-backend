f9dda7a398bc3cd30b9d5a19f52fc11d8e81d53f
Eric B <eric_baffour-addo@alumni.brown.edu>
2022-12-12 19:32:52 -0800

    Update pool to use heroku postgresql settings

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,11 @@
 const { Pool, Client } = require("pg");
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
+  user: process.env.PGUSERHEROKU,
+  host: process.env.PGHOSTHEROKU,
+  port: process.env.PGPORTHEROKU,
+  database: process.env.PGDATABASEHEROKU,
+  password: process.env.PGPASSWORDHEROKU,
 });
 
 //we are exporting this object, which has to function poolQuery and getClient. poolQuery in an SQL text statement and params/values and calls pool.query with those values which returns a promise with our results so we return this promise back to our main poolQuery function. We can import this object as db and do await db.poolQuery(). getClient calls pool.connect() which returns a promise that resolves with our client so we return this client back to our main getClient function. When we import, we can do const client=await db.getClient()

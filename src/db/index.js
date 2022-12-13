d959b4e2c73e468294430cfc6613cb58741d606f
Eric B <eric_baffour-addo@alumni.brown.edu>
2022-12-12 21:00:34 -0800

    Provide connectionString in newPool method

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,10 @@
 const { Pool, Client } = require("pg");
 
 const connectionString = process.env.DATABASE_URL;
-
+const pool = new Pool({
+  connectionString,
+});
+/*
 const pool = new Pool({
   user: process.env.PGUSERHEROKU,
   host: process.env.PGHOSTHEROKU,
@@ -9,7 +12,7 @@ const pool = new Pool({
   database: process.env.PGDATABASEHEROKU,
   password: process.env.PGPASSWORDHEROKU,
 });
-
+*/
 //we are exporting this object, which has to function poolQuery and getClient. poolQuery in an SQL text statement and params/values and calls pool.query with those values which returns a promise with our results so we return this promise back to our main poolQuery function. We can import this object as db and do await db.poolQuery(). getClient calls pool.connect() which returns a promise that resolves with our client so we return this client back to our main getClient function. When we import, we can do const client=await db.getClient()
 module.exports = {
   poolQuery: (text, params) => {

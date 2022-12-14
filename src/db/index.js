48fc16252e266064f71293b8c18648eee33fac35
Eric B <eric_baffour-addo@alumni.brown.edu>
2022-12-14 00:15:43 -0800

    Set back to production database connection string

--- src/db/index.js
+++ src/db/index.js
@@ -1,18 +1,21 @@
-const { Pool, Client } = require("pg");
+const { Pool} = require("pg");
 
 const connectionString = process.env.DATABASE_URL;
+
 const pool = new Pool({
   connectionString,
 });
-/*
+
+/* for loca connection to postgresql database
 const pool = new Pool({
-  user: process.env.PGUSERHEROKU,
-  host: process.env.PGHOSTHEROKU,
-  port: process.env.PGPORTHEROKU,
-  database: process.env.PGDATABASEHEROKU,
-  password: process.env.PGPASSWORDHEROKU,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
 });
 */
+
 //we are exporting this object, which has to function poolQuery and getClient. poolQuery in an SQL text statement and params/values and calls pool.query with those values which returns a promise with our results so we return this promise back to our main poolQuery function. We can import this object as db and do await db.poolQuery(). getClient calls pool.connect() which returns a promise that resolves with our client so we return this client back to our main getClient function. When we import, we can do const client=await db.getClient()
 module.exports = {
   poolQuery: (text, params) => {
